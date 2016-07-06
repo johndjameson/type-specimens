9388a5d2b496c79235ee668a39c43af7706418dc
John D. Jameson <thejohnjameson@gmail.com>
2016-07-05 21:21:56 -0400

    Install browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -10,9 +10,22 @@
 // -------------------------------------
 
 let gulp = require('gulp')
+let browserSync = require('browser-sync').create()
 
 // -------------------------------------
 //   Tasks
 // -------------------------------------
+
+// ----- Browser Sync ----- //
+
+gulp.task('bs', function() {
+  browserSync.init({
+    server: {
+      baseDir: './build'
+    }
+  })
+})
+
+// ----- Default ----- //
 
 gulp.task('default', function() {})
