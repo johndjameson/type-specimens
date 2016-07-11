a97959b1f9c03f4ebc13fd1806fa315d15090ae2
John D. Jameson <thejohnjameson@gmail.com>
2016-07-11 11:29:11 -0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,24 @@
+// *************************************
+//
+//  Gulpfile
+//
+// *************************************
 /* eslint-env node */
 
 let babelify = require('babelify')
-let browserSync = require('browser-sync').create()
 let browserify = require('browserify')
+let browserSync = require('browser-sync').create()
 let gulp = require('gulp')
+let gulpIf = require('gulp-if')
 let gulpUtil = require('gulp-util')
 let sass = require('gulp-sass')
+let uglify = require('gulp-uglify')
 let vinyl = require('vinyl-source-stream')
 let watchify = require('watchify')
 
-gulp.task('default', function () {
-  // process.env.NODE_ENV = 'production'
-})
+// -------------------------------------
+//  Functions
+// -------------------------------------
 
 function bundle(bundler) {
   return bundler
@@ -20,10 +27,27 @@ function bundle(bundler) {
     .on('error', function (error) {
       gulpUtil.log(error)
     })
+    .pipe(gulpIf('', uglify()))
     .pipe(vinyl('application.js'))
     .pipe(gulp.dest('build/javascripts/'))
     .pipe(browserSync.stream())
 }
+
+function isProduction() {
+  return process.env.NODE_ENV === 'production'
+}
+
+// -------------------------------------
+//  Functions
+// -------------------------------------
+
+// ----- Default ----- //
+
+gulp.task('default', function () {
+  // ...
+})
+
+// ----- Watch ----- //
 
 gulp.task('watch', function () {
   let watcher = watchify(browserify('./source/javascripts/main.jsx', watchify.args))
@@ -34,21 +58,21 @@ gulp.task('watch', function () {
 
   watcher.on('update', function () {
     bundle(watcher)
-    gulpUtil.log('updated')
   })
+
+  gulp.watch('source/stylesheets/**/*', ['sass'])
+
+  browserSync.init({
+    server: './build'
+  });
 })
 
-gulp.task('name',['tasks'], function() {
-    // content
-});
+// ----- Sass ----- //
 
 gulp.task('sass', function() {
-  gulp.src('source/stylesheets/**/*')
+  gulp
+    .src('source/stylesheets/**/*')
     .pipe(sass())
     .pipe(gulp.dest('build/stylesheets'))
     .pipe(browserSync.stream())
-})
-
-gulp.task('watch:sass', function () {
-  gulp.watch('source/stylesheets/**/*', ['sass'])
 })
