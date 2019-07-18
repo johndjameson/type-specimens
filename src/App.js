6c01b6b1a75275ba8695237315916318536febe3
John D. Jameson <thejohnjameson@gmail.com>
2019-07-18 08:59:45 -0500

    Revert testing and test again

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Check your code before you wreck your code 2
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
