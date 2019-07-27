d412c451155afee087b031c08db5303f0d3e8a86
John D. Jameson <thejohnjameson@gmail.com>
2019-07-26 21:47:42 -0500

    Add screenshot script

---
+++ scripts/screenshot.js
@@ -0,0 +1,28 @@
+const Pageres = require('pageres')
+const sharp = require('sharp')
+const fetch = require('node-fetch')
+
+const wait = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+const recordsUrl =
+  'https://api.airtable.com/v0/apptKHbxmAAcPuZMW/specimens?api_key=key3GGb7zxleGAfHl&filterByFormula=AND(Screenshot)&sortField=Slug'
+
+const takeScreenshot = async ({
+  fields: { URL: url, Delay: delay, Slug: slug },
+}) => {
+  await new Pageres({ delay, filename: `${slug}` })
+    .src(url, ['1200x900'], { crop: true })
+    .dest('screenshots')
+    .run()
+
+  await sharp(`screenshots/${slug}.png`)
+    .resize(600, 450)
+    .jpeg({ progressive: true, quality: 60 })
+    .toFile(`screenshots/${slug}-thumb.jpg`)
+}
+
+fetch(recordsUrl)
+  .then(response => response.json())
+  .then(({ records }) => {
+    records.forEach(takeScreenshot)
+  })
