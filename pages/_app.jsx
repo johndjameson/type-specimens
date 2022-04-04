a024b0685426a6ed105406b0b4bf71e34c1c25b9
John D. Jameson <thejohnjameson@gmail.com>
2022-04-04 11:33:23 -0500

    Move document tags to app

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,28 @@
+import Head from 'next/head';
+
 import 'sanitize.css';
 import 'sanitize.css/typography.css';
 import 'styles/base.css';
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+
+        <link rel="preconnect" href="https://ik.imagekit.io" />
+
+        <meta name="theme-color" content="#2e5094" />
+        <meta
+          content="A collection of the web’s best typeface specimens. Curated by John D. Jameson"
+          name="description"
+        />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default App;
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,17 +1,9 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en-US">
-      <Head>
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-
-        <meta name="theme-color" content="#2e5094" />
-        <meta
-          content="A collection of the web’s best typeface specimens. Curated by John D. Jameson."
-          name="description"
-        />
-      </Head>
+      <Head />
 
       <body>
         <Main />
