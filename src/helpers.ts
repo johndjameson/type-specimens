f68b39b81ad0d982dd6cfd97ac722dfa75999884
John D. Jameson <thejohnjameson@gmail.com>
2025-10-04 08:53:18 -0500

    Update image CDN to Cloudflare

--- src/helpers.ts
+++ src/helpers.ts
@@ -5,29 +5,3 @@ export const fetchJson = async (
   options: FetchOptions = {},
 ): Promise<any> =>
   await fetch(url, options).then((response) => response.json());
-
-interface ImageKitTransformations {
-  [key: string]: string | number;
-}
-
-interface ImageKitUrlParams {
-  path: string;
-  transformations?: ImageKitTransformations;
-}
-
-export const imageKitUrl = ({
-  path,
-  transformations = {},
-}: ImageKitUrlParams): string => {
-  const entries = Object.entries(transformations);
-
-  if (entries.length === 0) {
-    return `https://ik.imagekit.io/johndjameson/type-specimens/${path}`;
-  }
-
-  const transform = entries
-    .map(([param, value]) => `${param}-${value}`)
-    .join(",");
-
-  return `https://ik.imagekit.io/johndjameson/tr:${transform}/type-specimens/${path}`;
-};
